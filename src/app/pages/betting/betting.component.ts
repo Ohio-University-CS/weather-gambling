7c198af52f086116af0b0d07fd55d4d6d0a247b7
Ethan Knotts <eknotts64@gmail.com>
2025-11-13 17:47:24 -0500

    Fahrenheit

--- src/app/pages/betting/betting.component.ts
+++ src/app/pages/betting/betting.component.ts
@@ -18,7 +18,7 @@ interface DayBet {
   styleUrls: ['./betting.component.scss']
 })
 export class BettingComponent implements OnInit {
-  location: string = 'London';
+  location: string = 'Athens,OH,US';
   days: DayBet[] = [];
   selectedDay: DayBet | null = null;
   selectedOption: BetOption | null = null;
